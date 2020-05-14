4de5d855b3553138b93cfe5ac87d0b34398d8b1d
Grady Wicoady <nervalgravie@gmail.com>
2020-05-14 20:21:24 +0800

    deleteRoom worked

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,7 +1,10 @@
 const {Room} = require('../models')
+// const kue = require('kue')
+// const queue = kue.createQueue()
 
 class RoomController {
 
+    // TAMPILKAN LIST ROOM
     static roomList(cb){
         Room
             .findAll()
@@ -19,9 +22,9 @@ class RoomController {
         console.log(data.name)
 
         let newRoom = {
-            'name' : data.name,
+            'name' : data.name, //nama room
             players : {
-                ['1.' + data.name] : 0
+                ['1.' + data.playerName] : 0 // list pemain
             }
         }
 
@@ -37,21 +40,26 @@ class RoomController {
             })
     }
 
-    static deleteRoom(roomName,cb){
-        
+    // leave room
+    static leaveRoom(payload,cb){
+        console.log('delete??')
         Room
-            .destroy({where : {name : roomName}})
+            .destroy({where : {name : payload.roomName}})
             .then(results => {
+                console.log('room has deleted')
                 cb(null)
             })
             .catch(err => {
+                console.log(err)
                 cb()
             })
     }
 
+    //join room
     static joinRoom(roomName,cb){
 
     }
+
 }
 
 module.exports = RoomController
--- server.js
+++ server.js
@@ -5,36 +5,68 @@ const RoomController = require('./controllers/roomControllers')
 
 io.on('connection', function(socket){
 
-    socket.on('testing',function(){
-        console.log('tidur')
-    })
+    // socket.on('testing',function(){
+    //     console.log('tidur')
+    // })
 
+
+    // IN MENU
     console.log('bolu')
     //Buat Room
     socket.on('create-room', (data) => {
-        console.log('tidur')
+        // console.log('tidur')
         RoomController.createRoom(data, function(err,results){
             if(err){
                 socket.emit('show-error', 'unable to create room')
             }else{
-                io.emit('room-created', results)
-                console.log(results)
-                socket.join(results.name)
-                socket.emit('joining-room', {...results, playerKey: `1-${results.name}`, isHost : true})
+                io.emit('room-created', results) //send data ke room-created
+                // console.log(results)
+                socket.join(results.name) // join room yang dibuat
+                socket.emit('joining-room', {...results, playerKey: `1-${results.playerName}`, isHost : true}) 
             }
         })
     })
 
     //join room
-    socket.on('join-room', (roomDate) => {
-        RoomController.joinRoom(roomData, function(err, result){
+    socket.on('join-room', (payload) => {
+        RoomController.joinRoom(payload, function(err, results){
+            console.log('test')
             if(err){
-                socket.emit('display-error', 'unable to join' + result.roomName)
+                socket.emit('show-error', 'unable to join' + payload.roomName)
             }else {
-                socket.join()
+                socket.join(payload.roomName)
             }
         })
     } )
 
+    //show ROOM LISTs
+    socket.on('get-rooms', () => {
+        RoomController.roomList((err,results) => {
+            if(err){
+                socket.emit('show-error', 'failed read all rooms')
+            }else{
+                socket.emit('get-rooms', results) //mengirim data results ke get-rooms
+            }
+        })
+    })
+
+
+    // IN-GAME
+
+    socket.on('leave-room',(payload) => {
+        socket.leave(payload.roomName)
+        RoomController.leaveRoom(payload, (err,results) => {
+            io.to(payload.roomName).emit('player-left',results.players) //mengupdate data list player yang masih didalam room
+            io.emit('update-client-room')
+        })
+    })
+
+    socket.on('update-position',(payload) => {
+        socket.broadcast.to(payload.roomName).emit('update-position',payload) //trigger client untuk update posisi kuda
+    })
+
+    socket.on('update-score',(payload) => {
+        socket.broadcast.to(payload.roomName).emit('update-score',payload) // trigger client untuk mengupdate score
+    })
 })
 
